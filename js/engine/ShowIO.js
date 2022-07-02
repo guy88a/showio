b363826daa0aba1b1ac0e68ab55aa9f2a5941175
Guy Aston <guy88a@gmail.com>
2022-07-02 20:14:39 +0300

    conver ShowIO to js class

--- js/engine/ShowIO.js
+++ js/engine/ShowIO.js
@@ -1,78 +1,89 @@
 /* :::::::::::::::::::::::::::::::::::::::::: {SHOW IO} :::::::::::::::::::::::::::::::::::::::::::
-# GAME VERSION: 0.00.001
-- FILE VERSION: 1.00.001
+# GAME VERSION: 0.00.003
+- FILE VERSION: 1.01.002
 - INFO:
-    ...info
+    Game (Show) constructor.
+    Creates a new game instance with user's properties.
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
 
-/**
- * @param {Array}  dimensions           | default shows dimensions (width, height)
- * @param {Number} mobile               | adjust for mobile if value is 1
- * @param {Function} defalutConfigs     | general configurations
- * @param {Array}  defaultShowsArray    | array of shows configured ahead
- * @param {Array}  classicShowsArray    | array of another framework's games instances
- * 
- * @returns {Object}            | ShowIO's API
- */
-export default function ShowIO(
-    dimensions  = [1920, 1080],
-    mobile      = 0,
-    defalutConfigs = {},
-    defaultShowsArray = [],
-    classicShowsArray = [], //TODO: Implement
-){
-    
-    // =================================== MAIN CONFIGURATIONS ====================================
-    let configurations = defalutConfigs || {};
+class ShowIO {
+    /**
+     * 
+     * @param {String} _genre ............ GAME TYPE
+     * @param {Array} _dimensions ........ VIEW DIMENSIONS
+     * @param {Number} _mobile ........... IS MOBILE?
+     * @param {Object} _configurations ... GAME CONFIGURATIONS
+     * @param {Object} _controls ......... GAME CTONTROLS
+     * @param {Map} _scenes .............. GAME SCENES
+     * @param {Map} _assets .............. GAME ASSETS
+     */
+    constructor(
+        _genre          = 'FPS',
+        _dimensions     = [1920, 1080],
+        _mobile         = 0,
+        _configurations = function(){},
+        _controls       = function(){},
+        _scenes         = new Map(),
+        _assets         = new Map(),
+        ) {
+        // ======================================= SETTINGS =======================================
+        // # GANRE: Game type. determines base physicss, rules, controls, etc...
+        this.#genre          = '';
+        // # DIMENSIONS: Base game width and height.
+        this.#dimensions     = _dimensions;
+        // # MOBILE: 0 - False, 1 - Supported, 2 - Mobile only.
+        this.#mobile         = _mobile;
+        // # FRAME: Scene name, Frame ID.
+        this.#frame          = ['', 0];
+        // # CONFIGURATIONS: Game configurations - events, physics, rules, controls, audio, graphics, etc...
+        this.#configurations = _configurations;
+        // # CONTROLS: Custom game controls.
+        this.#controls       = _controls;
+        // # SCENES: Game scenes. handled via 'scenesManager'.
+        this.#scenes         = _scenes;
+        // # ASSETS: Game assets.
+        this.#assets         = _assets;
 
-    let activeShow = function(id = 0){};
-
-    let allShows = [...defaultShowsArray];
-
-    let showsManager = function(shows = []){};
-
-    // ======================================= MAIN METHODS =======================================
-    function prepareShow() {
-        //TODO: Implement
+        // ======================================= MANAGERS =======================================
+        // # SCENES MANAGER: Handle and maintain all game's scenes and renderers.
+        this.#scenesManager      = function(){};
+        // # INTERACTIVE MANAGER: Handle user interaction events.
+        this.#interactiveManager = function(){};
+        // # EVENTS_MANAGER: Handle general game events.
+        this.#eventsManager      = function(){};
     }
+    
+    /**
+     * :::: Private Properties Setup :::::::::::::::::
+     */
+    #genre;
+    #dimensions;
+    #mobile;
+    #frame;
+    #configurations;
+    #controls;
+    #scenes;
+    #assets;
+    #scenesManager;
+    #interactiveManager;
+    #eventsManager;
 
     /**
-     * Play Show
-     *  plays current active show
-     * @param {Number} frameId  | if exists, return to a saved show's frame
+     * :::: Show Directions ::::::::::::::::::::::::::
      */
-    function playShow(frameId = 0) {
-        return this.activeShow.play(frameId);
+    setup() {
+
     }
+    run() {
 
-    function cutCurrentShow() {
-        //TODO: Implement
     }
+    cut() {
 
-    function endShow(rameId = 0) {
-        //TODO: Implement
     }
+    end() {
 
-    /**
-     * Create New Show
-     *  ganerate a new game show
-     * @param {String} name     | name of new show
-     */
-    function createNewShow(name = 'New Show') {
-        return allShows.push(function(){});
     }
-
-    _.log("Hello Show!");
-    
-    // ========================================== RETURN ==========================================
-    return {
-        prepare: prepareShow,
-        run: playShow,
-        cut: cutCurrentShow,
-        end: endShow,
-
-        produceNewShow: createNewShow,
-    }
-
 }
+
+export default ShowIO;
 /* ShowIO Ends */
